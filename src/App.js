ed05f9d6f3a9afb2fb904620b86c8cffdf655b93
kesavan-K07 <kesavankubendran@gmail.com>
2023-09-29 12:50:28 +0530

    first add my project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <VideoEncoder></VideoEncoder> */}
+        <img src={"https://png.pngtree.com/png-vector/20230104/ourmid/pngtree-happy-birthday-circle-tag-golden-with-realistic-balloons-and-heart-vector-png-image_6551824.png "} className="App-logo" alt="logo" />
+        <p className='wish '>
+          Cheers to your personal new year!
+        </p>
+        <em className='wish'>Let's live it up</em>
+        <img src=""   />
+        <a
+          className="App-link"
+          href="https://www.instagram.com/p/CvAJMjDvAn7kblC2kUE46lcW8Hzl7r8v0lsUjA0/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         💫 Pavizha💫
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
